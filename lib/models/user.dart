c91d04f7b98ca69ffaedee4b1930764c40f4bc2a
jvcgnullbug <joaovictorgermain@gmail.com>
2023-07-12 21:42:43 -0300

    aula 02 - CRUD completo

---
+++ lib/models/user.dart
@@ -0,0 +1,13 @@
+class User {
+  final int? id;
+  final String name;
+  final String email;
+  final String avatarUrl;
+
+  const User({
+    this.id,
+    required this.name,
+    required this.email,
+    required this.avatarUrl,
+  });
+}
